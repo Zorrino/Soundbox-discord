89c17be952ad88c7a47b1e832b0b6dc66743fa84
Zorrino <41651765+Zorrino@users.noreply.github.com>
2018-11-13 19:56:04 +0100

    Add files via upload
    Ajout d'une 20aine de meme

--- index.js
+++ index.js
@@ -79,10 +79,318 @@ bot.on('message', function (message){
 		}
 	}
 	
-	if (message.content == 'rako perlinpinpin'){
+	if (message.content == 'rako russie'){
 		if (message.member.voiceChannel) {
 			message.member.voiceChannel.join()
-			broadcast.playFile('./perlinpinpin.mp3');
+			broadcast.playFile('./russie.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako camera'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./camera.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako RUN'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./RUN.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako 911'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./911.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako risitas'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./risitas.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako thomas'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./thomas.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako non'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./non.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako kaamelott'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./kaamelott.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako nils'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./nils.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == "rako C'est mon rêve"){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./C MON REVE.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako AHH'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./AHH.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako la boule magique'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./boule magique.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako calmez vous'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./calmez vous.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako faut le savoir'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./faut le savoir.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako triple'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./triple.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako j ai maaal'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./jai maaal.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako big burger'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./BIG BURGER.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako happen to me'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./happen to me.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako wombo combo'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./wombo combo.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako to be continued'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./TO BE CONTINUED.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako whats up fuckers'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./Whars up fuckers.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako see you again'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./see you again.mp3');
+			for (const connection of bot.voiceConnections.values()) {
+				connection.playBroadcast(broadcast);
+			}
+		}
+		 
+		else {
+			message.reply("Tu dois rejoindre un channel vocal d'abord !");
+		}
+	}
+	
+	if (message.content == 'rako ferme sa gueule et menage'){
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+			broadcast.playFile('./ferme sa gueule et menage.mp3');
 			for (const connection of bot.voiceConnections.values()) {
 				connection.playBroadcast(broadcast);
 			}
